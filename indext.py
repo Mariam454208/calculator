c98984dddd3d1bbc942179b1f9be8082e5ead462
Mariam454208 <146873848+Mariam454208@users.noreply.github.com>
2024-08-06 15:22:09 +0300

    Update indext.py

--- indext.py
+++ indext.py
@@ -6,12 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/calculate",methods=["post"])
-def calculate():
-    first_number= request.form["firstNumber"]
-    operation=request.form["operation"]
-    second_number=request.form["secondnumber"]
-    return "operation"
-
 if __name__ =="__main__":
     app.run(debug=True)
